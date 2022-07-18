3f118992d082a70699ce735b31d4bcf7066a9991
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-07-18 18:40:00 +0200

    tasks and operations

---
+++ src/Components/AddTask.js
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+
+export default function AddTask({onAddTask}) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+}
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import { auth, db, logout } from "./Firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import {getTasks} from "./ApiTasks"
+import AddTask from "./AddTask"
+import Task from "./Task"
 
 export default function HomePage() {
     const [user, setUser] = useAuthState(auth);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+     getTasks(setTasks);
+    },[])
+
+    const handleCreateTask = (task) => {
+        setTasks(prevState => {
+            return [
+                task,
+                ...prevState
+            ]
+        })
+    }
+
+    const handleRemoveTask = (id) => {
+        setTasks(prevState => prevState.filter(task => task.id !== id))
+    }
+
     return (
         <div className="homepage">
-            Welcome Stanger!!
-            <h1>Here are your tasks:</h1>
-            {getTasks()}
+            <AddTask onAddTask={handleCreateTask}/>
+            {tasks.map(task => {
+                return <Task key={task.id} {...task} onRemoveTask={handleRemoveTask}/>
+            })}
             <a href="/" className="nav__link" onClick={logout}>Wyloguj</a>
         </div>
     )
